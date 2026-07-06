6975e2bbe20a3fed81a6ce20c1ab47b408be7251
Saqib <syedsaqibali0786@gmail.com>
2026-07-07 03:14:36 +0530

    update

--- src/components/instagram/InstagramCard.jsx
+++ src/components/instagram/InstagramCard.jsx
@@ -37,7 +37,7 @@ const InstagramCard = ({ reel, onDelete }) => {
 
             <video
 
-                src={reel.video_url}
+                src={`https://pushpanjaliglobal.com${reel.video_url}`}
 
                 controls
 
